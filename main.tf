a3cc6c3735ee8cb1a3f507861e057881fa714b39
teerawat.yamsai <teerawat.yamsai@krungsri.com>
2020-07-11 18:00:23 +0700

    with module

--- main.tf
+++ main.tf
@@ -4,16 +4,34 @@ provider "aws" {
   profile = var.aws_profile
 }
 
-resource "aws_instance" "infra-property" {
-  ami           = "ami-02b6d9703a69265e9"
-  instance_type = "t2.micro"
 
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.infra-property.public_ip} > ip_address.txt"
-  }
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.21.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  tags = var.vpc_tags
 }
 
-resource "aws_eip" "ip" {
-  vpc = true
-  instance = aws_instance.infra-property.id
+module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.12.0"
+
+  name           = "infra-prop-ec2-cluster"
+  instance_count = 1
+
+  ami                    = var.ec2_ami
+  instance_type          = var.ec2_instance_type
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = var.ec2_tags
 }
