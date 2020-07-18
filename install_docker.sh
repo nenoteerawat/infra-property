0b880ac68d4beecd92e5025818ed0874b7302dea
teerawat.yamsai <teerawat.yamsai@krungsri.com>
2020-07-18 19:51:05 +0700

    fix port

--- install_docker.sh
+++ install_docker.sh
@@ -5,4 +5,4 @@ service docker start
 usermod -a -G docker ec2-user
 chmod 666 /var/run/docker.sock
 docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
-docker run -d -p 8080:80 --name web-management-property --restart always nenoteerawat/management-property:latest
+docker run -d -p 80:8080 --name web-management-property --restart always nenoteerawat/management-property:latest
