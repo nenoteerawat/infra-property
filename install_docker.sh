ec2665f7c7b153cf2a944e8fe2530e3abbd42c2d
teerawat.yamsai <teerawat.yamsai@krungsri.com>
2020-07-18 19:44:28 +0700

    add start docker web management property when init aws ec2

--- install_docker.sh
+++ install_docker.sh
@@ -5,3 +5,4 @@ service docker start
 usermod -a -G docker ec2-user
 chmod 666 /var/run/docker.sock
 docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+docker run -d -p 8080:80 --name web-management-property --restart always nenoteerawat/management-property:latest
