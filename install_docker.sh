60b3fe2df020c3eca17c3f619412959db65f5cc3
teerawat.yamsai <teerawat.yamsai@krungsri.com>
2020-07-18 16:04:23 +0700

    finish infra

--- install_docker.sh
+++ install_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-yum update -y
-yum install docker
+yum -y update
+yum -y install docker
 service docker start
 usermod -a -G docker ec2-user
 chmod 666 /var/run/docker.sock
-docker info
+docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
