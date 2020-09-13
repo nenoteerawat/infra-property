f4c0c235a1485038921e82d8f0727638fee2e669
teerawat.yamsai <teerawat.yamsai@krungsri.com>
2020-09-13 20:08:49 +0700

    implement approve

--- install_docker.sh
+++ install_docker.sh
@@ -5,4 +5,6 @@ service docker start
 usermod -a -G docker ec2-user
 chmod 666 /var/run/docker.sock
 docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
-docker run -d -p 80:8080 --name web-management-property --restart always nenoteerawat/management-property:latest
+docker run -d -p 8080:8080 --name web-management-property --restart always property123/management-property:latest
+docker run -d -p 8090:8090 --name api-management-property --restart always property123/api-manage-property:latest
+docker run -d -p 27017:27017 --name mongodb --restart always -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=p@ssw0rd -v mongo_data:/data/db -v monog_config:/data/configdb mongo:latest
--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_network_interface" "property-network-interface" {
   }
 }
 
-resource "aws_instance" "property-ec2" {
+resource "aws_instance" "property_ec2" {
   key_name = aws_key_pair.property.key_name
   ami = var.ec2_ami
   instance_type = var.ec2_instance_type
@@ -34,4 +34,47 @@ resource "aws_instance" "property-ec2" {
     device_index = 0
   }
 
+}
+
+resource "aws_s3_bucket" "property_s3" {
+  bucket = "property-s3"
+  acl    = "public-read"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+
+  tags = {
+    Name        = "Property S3"
+  }
+}
+
+resource "aws_s3_bucket_policy" "property_s3_bucket_policy" {
+  bucket = aws_s3_bucket.property_s3.id
+  policy = <<POLICY
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Sid":"PublicRead",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject","s3:GetObjectVersion"],
+      "Resource":["arn:aws:s3:::${aws_s3_bucket.property_s3.bucket}/*"]
+    }
+  ]
+}
+  POLICY
+}
+
+resource "aws_s3_bucket_public_access_block" "property_s3_public_access_block" {
+  bucket = aws_s3_bucket.property_s3.id
+//  block_public_acls = true
+//  block_public_policy = true
+//  ignore_public_acls = true
+//  restrict_public_buckets = true
 }
